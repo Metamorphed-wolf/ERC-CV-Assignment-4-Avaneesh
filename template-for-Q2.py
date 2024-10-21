db1b436f07fb954d30956d30f267658602b9d300
Metamorphed-wolf <f20240835@goa.bits-pilani.ac.in>
2024-10-21 23:31:26 +0530

    Update template-for-Q2.py

--- template-for-Q2.py
+++ template-for-Q2.py
@@ -4,25 +4,34 @@ import numpy as np
 import random
 
 # Initialize MediaPipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
 
 
 # Game settings
 width, height = 1280, 640
 player_pos = [320, 440]
 # enemy speed, size, and list initialization
-
+enemy_speed=5
+enemy_size=40
+enemy_list=[]
 
 # Initialize score
-
+score=0
 
 # Create random enemy
 def create_enemy():
+    x_coordinate=np.random.randint(0,width - enemy_size)
+    return [x_coordinate,0]
     
 
 # Move enemies down
 def move_enemies(enemy_list):
+    for enemy in enemy_list:
+        enemy[1]+=enemy_speed
     
 # Check if enemy is off-screen
+def check_off
 # Increment score for each enemy that goes off-screen
 
 
